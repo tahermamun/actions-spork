0b0826d2c2416e410cf7a755f60804a30f234691
TAHER MAMUN <tahermamun5@gmail.com>
2026-02-06 22:34:52 +0000

    feat: test  workflow

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+server.listen(3000, () => {
+  console.log(`Example app listening on port ${3000}`)
+});
---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+
+const app  = require('../app');
+
+describe("GET /",function(){
+    it('responds with text "Hello World!"',async function (){
+        const chai= await import("chai");
+        const expect = chai.expect;
+
+        await request(app)
+            .get('/')
+            .expect(200)
+            .then((res)=>{
+                expect(res.text).to.equal("Hello World!")
+            })
+    })
+})
